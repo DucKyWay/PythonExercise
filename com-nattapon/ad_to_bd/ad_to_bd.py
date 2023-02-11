5025acf9e3c4f44ff690cf6f152debb69424b738
Suphawinee Chatasawapreeda <niji2549@gmail.com>
2023-02-12 00:18:55 +0700

    change ad_to_bd and add some clock

--- com-nattapon/ad_to_bd/ad_to_bd.py
+++ com-nattapon/ad_to_bd/ad_to_bd.py
@@ -1,24 +1,19 @@
 ad_year = int(input())
 bd_year = ad_year + 543
 
-str_ad = str(ad_year)
-str_bd = str(bd_year)
+str_ad, str_bd = str(ad_year), str(bd_year)
+each_ad_num, each_bd_num = [], []
+ad_ans, bd_ans = 0, 0
 
-each_ad_num = []
-each_bd_num = []
-ad_ans = 0
-bd_ans = 0
-
-for i in str_ad:
+for i, j in zip(str_ad, str_bd):
     each_ad_num.append(int(i))
-
-for i in str_bd:
-    each_bd_num.append(int(i))
+    each_bd_num.append(int(j))
 
 for i in str_ad:
     for j in str(i):
         ad_ans += int(j)
     each_ad_num.append(ad_ans)
+
 for i in str_bd:
     for j in str(i):
         bd_ans += int(j)
