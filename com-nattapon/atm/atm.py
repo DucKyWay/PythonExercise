ebb707415052c9eb0848e44ae62e7ec5c010dadf
Suphawinee Chatasawapreeda <niji2549@gmail.com>
2023-02-12 20:44:39 +0700

    catch up

--- com-nattapon/atm/atm.py
+++ com-nattapon/atm/atm.py
@@ -10,14 +10,20 @@ if money_1 <= 20000:
     if money_2 <= 20000:
         money_sum = money_1 + money_2
         if money_sum % 100 == 0:
-            if money_sum >= 1000:
+            if money_sum >= 100:
                 moneyCash = money_sum / 1000
                 moneyOver = moneyCash-20
+                print("money over: ", moneyOver)
                 if moneyOver > 0:
                     moneyCash -= moneyOver
                     money_sum += (moneyOver * 1000)
+                    print("sum : ", money_sum)
+                    print("money over", moneyOver)
                 print(int(moneyCash))
-                money_sum %= 1000
+                print("test : ", money_sum)
+                money_sum //= 1000
+
+                print("test ", money_sum)
 
                 moneyCash = money_sum / 500
                 moneyOver = moneyCash - 20
@@ -25,7 +31,7 @@ if money_1 <= 20000:
                     moneyCash -= moneyOver
                     money_sum += (moneyOver * 500)
                 print(int(moneyCash))
-                money_sum %= 500
+                money_sum //= 500
 
                 moneyCash = money_sum / 100
                 moneyOver = moneyCash - 20
@@ -33,7 +39,7 @@ if money_1 <= 20000:
                     moneyCash -= moneyOver
                     money_sum += (moneyOver * 100)
                 print(int(moneyCash))
-                money_sum %= 100
+                money_sum //= 100
         else:
             print("Input Error")
     else:
