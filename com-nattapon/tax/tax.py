99f6b8321d48991ea9b194ee7cb7d268a1e2ef69
Suphawinee Chatasawapreeda <niji2549@gmail.com>
2023-02-14 06:04:36 +0700

    add tax files

---
+++ com-nattapon/tax/tax.py
@@ -0,0 +1,10 @@
+list = [0, 0, 0, 0]
+list[0] = int(input('1: '))
+list[1] = int(input('2: '))
+list[2] = int(input('3: '))
+list[3] = int(input('4: '))
+
+i = 0
+for i in range(4):
+    print(i)
+    i += 1
