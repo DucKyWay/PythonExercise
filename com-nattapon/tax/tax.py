366ff83a041fe6f39775f0b2261c142d4973105e
Suphawinee Chatasawapreeda <80985006+DucKyWay@users.noreply.github.com>
2023-02-16 04:04:30 +0700

    what git branch buggggg

--- com-nattapon/tax/tax.py
+++ com-nattapon/tax/tax.py
@@ -1,10 +1,22 @@
 list = [0, 0, 0, 0]
-i, j = 0
+list[0] = int(input('1: '))
+list[1] = int(input('2: '))
+list[2] = int(input('3: '))
+list[3] = int(input('4: '))
+
+tax = [0, 0, 0, 0]
+i = 0
 
 for i in range(4):
-    list[i] = int(input(i, ': '))
+    if list[i] < 100000:
+        tax[i] = 0
+    if list[i] >= 100000 and list[i] <= 500000:
+        tax[i] = list[i] * (5/100)
+    elif list[i] >= 500001 and list[i] <= 1000000:
+        tax[i] = list[i] * (10/100)
+    elif list[i] > 1000000:
+        tax[i] = list[i] * (20/100)
     i += 1
 
-for j in range(4):
-    print(i, "=", list[i])
-    i += 1
+total_tax = sum(tax)
+print(f'{total_tax:.2f}')
