f089d2060c690bc36bb74b04e35f2c1125c2b3c2
Suphawinee Chatasawapreeda <niji2549@gmail.com>
2023-02-17 05:56:32 +0700

    sequenceVII

--- ejudge/sequenceVII.py
+++ ejudge/sequenceVII.py
@@ -1,11 +1,9 @@
-def main():
-    value = int(input())
-    for line in range(1, value+1):
-        for row in range(line):
-            print(row+1, end=" ")
-        print()
-    for line in range(value-1, 0, -1):
-        for row in range(line):
-            print(row+1, end=" ")
-        print()
-main()
+value = int(input())
+for line in range(1, value+1):
+    for row in range(line):
+        print(row+1, end=" ")
+    print()
+for line in range(value-1, 0, -1):
+    for row in range(line):
+        print(row+1, end=" ")
+    print()
