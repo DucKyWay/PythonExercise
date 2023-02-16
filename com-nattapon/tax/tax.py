c7f15314b675e371167a5d88908e6b1d8e88d564
Suphawinee Chatasawapreeda <niji2549@gmail.com>
2023-02-16 09:46:55 +0700

    if > 0

--- com-nattapon/tax/tax.py
+++ com-nattapon/tax/tax.py
@@ -8,14 +8,17 @@ tax = [0, 0, 0, 0]
 i = 0
 
 for i in range(4):
-    if list[i] < 100000:
-        tax[i] = 0
-    elif list[i] >= 100000 and list[i] <= 500000:
-        tax[i] = list[i] * (5/100)
-    elif list[i] >= 500001 and list[i] <= 1000000:
-        tax[i] = list[i] * (10/100)
-    elif list[i] > 1000000:
-        tax[i] = list[i] * (20/100)
+    if list[i] >= 0:
+        if list[i] < 100000:
+            tax[i] = 0
+        elif list[i] >= 100000 and list[i] <= 500000:
+            tax[i] = list[i] * (5/100)
+        elif list[i] >= 500001 and list[i] <= 1000000:
+            tax[i] = list[i] * (10/100)
+        elif list[i] > 1000000:
+            tax[i] = list[i] * (20/100)
+    else:
+        break
 
 total_tax = sum(tax)
 print(f'{total_tax:.2f}')
